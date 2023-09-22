ef5401b9be70d33167412fb8862914984f1b32a5
葉晨 <denny3388@gmail.com>
2023-09-23 00:05:25 +0800

    add freeze Qformer bash

--- run_scripts/textual-inversion/train_freezeQformer.sh
+++ run_scripts/textual-inversion/train_freezeQformer.sh
@@ -1,14 +1,14 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-# export CUDA_VISIBLE_DEVICES=4,5,6,7
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 export CUDA_HOME=/usr/local/cuda-11.4
 
 # python -m torch.distributed.run --nproc_per_node=2 /home/denny/LAVIS/train.py --cfg-path /home/denny/LAVIS/lavis/projects/textual-inversion/textinv_freezeLLM_train.yaml
 
 torchrun \
     --nnodes=1 \
-    --nproc_per_node=4 \
+    --nproc_per_node=3 \
     --rdzv_backend c10d \
-    --rdzv_endpoint localhost:25701 \
-    /home/denny/LAVIS/train.py --cfg-path /home/denny/LAVIS/lavis/projects/textual-inversion/textinv_freezeLLM_train.yaml
+    --rdzv_endpoint localhost:25703 \
+    /home/denny/LAVIS/train.py --cfg-path /home/denny/LAVIS/lavis/projects/textual-inversion/textinv_freezeQformer_train.yaml
 
     # --standalone \
